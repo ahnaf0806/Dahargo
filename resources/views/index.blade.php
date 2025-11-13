a22e1996ef350f6c18afad6e6ab32ab3072f84cf
ahnaf0806 <ahnafogy81@gmail.com>
2025-11-13 17:44:10 +0700

    tes boostrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Dahargo</title>
 
-        @vite(['resource/sass/app.scss'])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body class="bg-primary"></body>
 </html>
