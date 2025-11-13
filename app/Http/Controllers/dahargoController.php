6c9dae5d3dfa26810e43f1075e1b05ec2b210946
ahnaf0806 <ahnafogy81@gmail.com>
2025-11-13 17:40:14 +0700

    tes

--- app/Http/Controllers/dahargoController.php
+++ app/Http/Controllers/dahargoController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class dahargoController extends Controller
 {
-    //
+    public function index() {
+        return view('index');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\dahargoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [dahargoController::class, "index" ]);
